aceb9d9fe54c249ca8b577ac64b3dfe1899d007b
ConnorBrereton <breretonconnor@gmail.com>
2018-09-15 04:55:24 +0000

    implement a character array holding the string. use write() to write to stderr the string and the byte size of the string

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - entry point for program
@@ -10,8 +10,8 @@
 
 int main(void)
 {
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	printf("%s\n", str);
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, str, 59);
 
 	return (1);
 }
