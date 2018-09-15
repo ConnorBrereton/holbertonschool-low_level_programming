9337d01c0c61774db89d9456d1018b8a364a589d
ConnorBrereton <breretonconnor@gmail.com>
2018-09-15 04:28:25 +0000

    store quote in a character array and then print the array

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point for program
+ *
+ * Description: print a string using a character array
+ *
+ * Return: always 1 (success)
+ */
+
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	printf("%s\n", str);
+
+	return (1);
+}
