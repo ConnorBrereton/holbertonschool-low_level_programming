bc8a36abe0c432c8d7e62657bc5f63c7cffd5575
ConnorBrereton <breretonconnor@gmail.com>
2018-09-17 21:38:04 +0000

    use a character array and while loop to print low-alpha 10x without standard library

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * desc - print low-alpha 10x
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i, count = 9;
+	char c[] = "abcdefghijklmnopqrstuvqxyz";
+
+	while (count > -1)
+	{
+		for (i = 0; i < 27; i++)
+			_putchar(c[i]);
+		_putchar('\n');
+		count--;
+	}
+	return (0);
+}
