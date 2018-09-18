d9233898ada5bc8a8ceac08507a7fcce765ef820
ConnorBrereton <breretonconnor@gmail.com>
2018-09-18 21:56:38 +0000

    prints EU time minute by minute without use of time library

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,52 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - Prints full day EU time min by min
+ *
+ * @left-minute: 00:10
+ *
+ * @right-minute: 00:01
+ *
+ * @left-hour: 10:00
+ *
+ * @right-hour: 01:00
+ *
+ * description: Displays minbymin EU time w/o time lib
+ *
+ * Return: Void fcn so no value
+ */
+
+void jack_bauer(void)
+{
+	int left_minute, right_minute, left_hour, right_hour;
+
+	left_hour = 0;
+	while (left_hour <= 2)
+	{
+		right_hour = 0;
+
+		while (right_hour < 4)
+		{
+			if (right_hour > 3 && left_hour == 2)
+				break;
+			left_minute = 0;
+			while (left_minute < 6)
+			{
+				right_minute = 0;
+				while (right_minute < 10)
+				{
+					_putchar(left_hour + '0');
+					_putchar(right_hour + '0');
+					_putchar(58);
+					_putchar(left_minute + '0');
+					_putchar(right_minute + '0');
+					_putchar('\n');
+					right_minute++;
+				}
+				left_minute++;
+			}
+			right_hour++;
+		}
+		left_hour++;
+	}
+}
