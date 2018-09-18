e2cde82294771d2c7851dfad5b0803b09d5be8f5
ConnorBrereton <breretonconnor@gmail.com>
2018-09-18 18:20:09 +0000

    changed typo in alphabet hardcoding

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,13 +11,13 @@
 void print_alphabet_x10(void)
 {
 	int i, count = 9;
-	char c[] = "abcdefghijklmnopqrstuvqxyz";
+	char c[] = "abcdefghijklmnopqrstuvwxyz";
 
 	while (count > -1)
 	{
 		for (i = 0; i < 27; i++)
 			_putchar(c[i]);
-		_putchar('\n');
 		count--;
+		_putchar('\n');
 	}
 }
