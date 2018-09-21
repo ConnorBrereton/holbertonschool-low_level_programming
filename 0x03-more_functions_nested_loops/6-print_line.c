163743f359058707057ccd5182ab93b830422aa2
ConnorBrereton <breretonconnor@gmail.com>
2018-09-21 05:34:33 +0000

    added line function and removal of whitespace script

---
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_line - puts 'n' lines
+ *
+ * @n: passed as argument
+ *
+ * Return: none
+ */
+
+void print_line(int n)
+{
+	int i;
+	
+	for (i = 0; i < n; i++)
+	{
+		if (n > 0)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
