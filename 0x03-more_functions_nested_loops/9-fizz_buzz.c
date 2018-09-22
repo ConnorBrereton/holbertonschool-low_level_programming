7b974b1e6a165e1382cb17c42bf3ab00f2eff453
ConnorBrereton <breretonconnor@gmail.com>
2018-09-22 04:50:44 +0000

    implemented fizz buzz with standard lib

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - implements fizz buzz
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz ");
+
+		else if (i % 5 == 0)
+			printf("Buzz ");
+
+		else if (i % 15 == 0)
+			printf("FizzBuzz ");
+
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
