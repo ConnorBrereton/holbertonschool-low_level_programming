b86e141fae6658591951fdf76089761650c00098
ConnorBrereton <breretonconnor@gmail.com>
2018-09-26 01:08:23 +0000

    prints string in reverse using pointers but DOESNT affect memory

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_rev - print in reverse dont change memory
+ *
+ * @s: char pointer to string
+ *
+ * Return: none
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	for (i = i - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
