64f682f62f7e38ddad0445e9eab07040ad1f4acf
ConnorBrereton <breretonconnor@gmail.com>
2018-09-28 04:13:51 +0000

    fixed error where @src was not being read from

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -15,7 +15,7 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 	int end = 0;
 
-	for (i = 0; i < n && dest[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
