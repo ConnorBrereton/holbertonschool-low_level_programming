d0c8e8847eb34afd687f126c825b1e73c8e29f05
ConnorBrereton <breretonconnor@gmail.com>
2018-09-28 22:05:30 +0000

    implemented toupper function

---
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+
+/**
+ * *string_toupper - converts lowercase to uppercase
+ *
+ * @s: pointer to string
+ *
+ * Return: 0-failure, s-string
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	if (*s == '\0')
+		return (0);
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' &&  s[i] <= 'z')
+			s[i] -= 32;
+	}
+	return (s);
+}
