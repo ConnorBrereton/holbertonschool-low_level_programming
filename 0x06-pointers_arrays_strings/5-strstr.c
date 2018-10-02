bd9cc3af46122471f05dbd95ef8331f230818689
ConnorBrereton <breretonconnor@gmail.com>
2018-10-02 04:43:19 +0000

    implemented strstr() from scratch

---
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,41 @@
+#define NULL 0
+/**
+ * match - loops both arrays and breaks
+ * @haystack: memory area pointed to.
+ * @needle: constant byte to fill memory with.
+ * Return: pointer to beginning of located substring
+ *              or NULL if not found.
+ */
+
+int match(char *haystack, char *needle)
+{
+	while (*haystack && *needle)
+	{
+		if (*haystack != *needle)
+			return (0);
+
+		haystack++;
+		needle++;
+	}
+	return (*needle == '\0');
+}
+
+
+/**
+ * _strstr - locates a substring in given string
+ * @haystack: memory area pointed to.
+ * @needle: constant byte to fill memory with.
+ * Return: pointer to beginning of located substring
+ *		or NULL if not found.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		if ((*haystack == *needle) && match(haystack, needle))
+			return (haystack);
+		haystack++;
+	}
+	return (NULL);
+}
