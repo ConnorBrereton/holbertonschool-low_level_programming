67fd20a1c4d16dbf8d705ba3ff033df28554932f
ConnorBrereton <breretonconnor@gmail.com>
2018-10-02 00:31:17 +0000

    implemented strchr() without std lib

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#define NULL 0
+
+/**
+ * *_strchr - strchr() w/o stdlib
+ * @s: pointer to string
+ * @c: character to match
+ * Return: string from @c to \0
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if ((char) c == s[i]) /* found char */
+		{
+			return ((char *) &s[i]); /* returns contents @ position */
+		}
+	}
+	return (NULL);
+}
