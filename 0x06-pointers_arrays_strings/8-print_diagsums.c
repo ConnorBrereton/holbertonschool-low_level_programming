30dcd36f4ef209abbaa5bc7999d1fca1c824d4fb
ConnorBrereton <breretonconnor@gmail.com>
2018-10-03 19:26:18 +0000

    implemented program to run operations on 2D array as 1D array in memory

--- 0x06-pointers_arrays_strings/8-print_diagsums.c
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -1,10 +1,9 @@
-#include "holberton.h"
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- * @a: pointer to array
- * @size: mxn size
+ * print_diagsums - map 2D->1D array and add up pattern
+ * @a: pointer to array in memory
+ * @size: row/column size
  * Return: none
  */
 
@@ -14,12 +13,17 @@ void print_diagsums(int *a, int size)
 	int sum1 = 0;
 	int sum2 = 0;
 
-	for (i = 0; i < size * size; i = i + (size + 1)) /* confines rows */
+	/* get a[0] a[4] a[6] pattern at scale */
+	for (i = 0; i < size * size; i = i + (size + 1))
+	{
 		sum1 += a[i];
-		
-	for (j = 0; j < size * size; j = j + (size - 1)) /* confines columns */
-		sum2 += a[j];
+	}
 
+	/* get a[2] a[4] a[6] pattern at scale */
+	for (j = size - 1; j < size * size - 1; j = j + (size - 1))
+	{
+		sum2 += a[j];
+	}
 
 	printf("%d, ", sum1);
 	printf("%d\n", sum2);
