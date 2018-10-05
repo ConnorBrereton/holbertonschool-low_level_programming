ebba4d549ae95b89f06f505b5ea67a2bac416487
ConnorBrereton <breretonconnor@gmail.com>
2018-10-05 18:01:33 +0000

    added unused variable attribute to avoid compilation errors

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: pointer to array that holds the arguments
  */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__ ((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 }
