22f89e8fa5de8aa586846a03d4bce9114caac57e
ConnorBrereton <breretonconnor@gmail.com>
2018-10-07 22:21:02 +0000

    added function to print the number of arguments that are passed into main

--- 0x09-argc_argv/1-args.c
+++ 0x09-argc_argv/1-args.c
@@ -4,18 +4,14 @@
  * main - prints the number of arguments passed into program
  * @argc: the size of the array of arguments
  * @argv: pointer to array that holds arguments
+ * Return: 0 (success) -1 (failure)
  */
 
 int main(int argc, char *argv[])
 {
-	
-	if (!*argv)
-	{
-		return (0);
-	}
+	if (*argv == '\0')
+		return (-1);
 
-	else
-	{
-		printf("%d\n", argc);
-	}
+	printf("%d\n", argc - 1);
+	return (0);
 }
