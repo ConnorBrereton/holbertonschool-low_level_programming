4ec15512eff948eb4c8388c48a83f4407f61e8a8
ConnorBrereton <breretonconnor@gmail.com>
2018-10-15 22:10:29 +0000

    defined the dog struct in the header file

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - struct for dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: ADT for dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_H */
