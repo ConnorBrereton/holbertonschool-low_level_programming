de615cbd86083a13d3dd69971610c622e04e8e1c
ConnorBrereton <breretonconnor@gmail.com>
2018-10-16 00:46:06 +0000

    created function that checks each member of struct and replaces NULL member with (nil)

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -17,7 +17,8 @@ struct dog
 	char *owner;
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
 int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif /* _DOG_H */
