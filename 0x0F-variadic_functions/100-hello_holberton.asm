fb176aab76b287ad444935e0d1dec2b643ade66c
ConnorBrereton <breretonconnor@gmail.com>
2018-10-21 08:37:11 +0000

    removed comments since that is causing compilation errors

--- 0x0F-variadic_functions/100-hello_holberton.asm
+++ 0x0F-variadic_functions/100-hello_holberton.asm
@@ -1,14 +1,14 @@
 	global _start
 
 	section .text
-_start: mov	rax, 1			; put 1 into temporary register
-	mov	rdi, 1			; write to stdout
-	mov	rsi, message		; passes address of string to stdout
-	mov	rdx, 17			; allocating 17 bytes for our string
-	syscall				; OS will write
-	mov	rax, 60			; exit
-	xor	rdi, rdi		; 1 XOR 1 = 0 so exit code is '0'
+_start: mov	rax, 1
+	mov	rdi, 1
+	mov	rsi, message
+	mov	rdx, 17
+	syscall
+	mov	rax, 60
+	xor	rdi, rdi
 	syscall
 
 	section .data
-message: db	"Hello, Holberton", 10	; 10 is '\n'
+message: db	"Hello, Holberton", 10
