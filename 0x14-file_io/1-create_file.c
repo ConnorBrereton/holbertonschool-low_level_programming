8daf18e745f96b2e48fa445c759b6e32b17e5176
ConnorBrereton <breretonconnor@gmail.com>
2018-11-09 01:05:51 +0000

    Eliminated unneeded pointer to improve space performance and fix buffer write errors

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -16,27 +16,22 @@ int create_file(const char *filename, char *text_content)
 {
 	int i, check_open, check_write;
 
-	char *text = text_content;
-
 	check_open = check_write = 0;
 
 	if (!filename)
 		return (-1);
 
-	check_open = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	check_open = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
 	if (check_open < 0)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		for (i = 0; *text; text++)
-			i++;
+	while (text_content && *(text_content + i))
+		i++;
 
-		check_write = write(check_open, text, i);
-	}
+	check_write = write(check_open, text_content, i);
 
-	if (close(check_open) < 0 || i != check_write)
+	if (close(check_open) < 0 || i != check_write || check_write < 0)
 		return (-1);
 
 	return (1);
