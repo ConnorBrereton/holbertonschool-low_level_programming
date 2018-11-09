d2932a91f5cb520d2ae48051a112499823fb9dd9
ConnorBrereton <breretonconnor@gmail.com>
2018-11-09 01:17:41 +0000

    Moved the order of the open macros so that when opened it automatically is set to the end of the file

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -15,14 +15,12 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int i, check_open, check_write;
 
-	char *text = text_content;
-
 	check_open = check_write = 0;
 
 	if (!filename)
 		return (-1);
 
-	check_open = open(filename, O_WRONLY | O_APPEND);
+	check_open = open(filename, O_APPEND | O_WRONLY);
 
 	if (check_open < 0)
 		return (-1);
@@ -33,13 +31,10 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (1);
 	}
 
-	if (text_content != NULL)
-	{
-		for (i = 0; *text; text++)
-			i++;
+	while (text_content && *(text_content + i))
+		i++;
 
-		check_write = write(check_open, text, i);
-	}
+	check_write = write(check_open, text_content, i);
 
 	if (close(check_open) < 0 || i != check_write)
 		return (-1);
