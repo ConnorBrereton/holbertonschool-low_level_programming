dcb26141593e303dc051758e8c68f0f1fc126c9c
ConnorBrereton <breretonconnor@gmail.com>
2018-12-05 02:06:43 +0000

    Removed all spacing to fix errors

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python
-
-
 import random
 number = random.randint(-10000, 10000)
-
 if number < 0:
     ld = number % -10
-
 else:
     ld = number % 10
-
 print("Last digit of {:d} is {:d} ".format(number, ld), end="")
-
 if ld > 5:
     print("and is greater than 5")
 elif ld == 0:
