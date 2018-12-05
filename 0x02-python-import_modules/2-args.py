335ea391518ff494b7e05a1fbb8c297fcd5ae14e
ConnorBrereton <breretonconnor@gmail.com>
2018-12-05 22:32:06 +0000

    Removed typo after the counter printed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
             print("1 argument:")
 
         else:
-            print("{}: arguments:".format(args))
+            print("{} arguments:".format(args))
 
         for i in range(1, args + 1):
             print("{}: {}".format(i, argv[i]))
