38d20f3381579b55ba9853dde5d010d18b3556a8
ConnorBrereton <breretonconnor@gmail.com>
2018-12-09 19:44:12 +0000

    Function that prints all nodes and returns count

---
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <unistd.h>
+
+/**
+ * print_dlistint - Prints member of each node
+ *
+ * @h: Pointer to the first node in SLL
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *curr = h;
+	size_t qty = 0;
+
+	if (!curr)
+		return (qty);
+
+	while (curr)
+	{
+		printf("%d\n", curr->n);
+		curr = curr->next;
+		qty++;
+	}
+
+	return (qty);
+}
