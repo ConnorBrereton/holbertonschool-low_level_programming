f17d758d6046137f29ce57dff935a4f556d5bb81
ConnorBrereton <breretonconnor@gmail.com>
2018-12-10 01:51:52 +0000

    Changed return to be Betty compliant

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -22,7 +22,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	cur = *h;
 
 	if (idx == 0)
-		return(add_dnodeint(h, n));
+		return (add_dnodeint(h, n));
 
 	if (!cur)
 		return (NULL);
