0257292bd3f57cbe32640df6ea91f9b8ba137af2
ConnorBrereton <breretonconnor@gmail.com>
2018-12-11 01:31:02 +0000

    Compacted all of the C source files into one directory

---
+++ 0x17-dynamic_libraries/src/5-strstr.c
@@ -0,0 +1,44 @@
+#define NULL 0
+/**
+ * match - loops both arrays and breaks
+ * @haystack: memory area pointed to.
+ * @needle: constant byte to fill memory with.
+ * Return: pointer to beginning of located substring
+ *              or NULL if not found.
+ */
+
+int match(char *haystack, char *needle)
+{
+	while (*haystack && *needle)
+	{
+		if (*haystack != *needle)
+			return (0);
+
+		haystack++;
+		needle++;
+	}
+	return (1);
+}
+
+
+/**
+ * _strstr - locates a substring in given string
+ * @haystack: memory area pointed to.
+ * @needle: constant byte to fill memory with.
+ * Return: pointer to beginning of located substring
+ *		or NULL if not found.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	if (!*needle)
+		return (haystack);
+
+	while (*haystack != '\0')
+	{
+		if ((*haystack == *needle) && match(haystack, needle))
+			return (haystack);
+		haystack++;
+	}
+	return (NULL);
+}
