c9003dda30c82e6eed33b3fdc6c4682f596fb112
ConnorBrereton <breretonconnor@gmail.com>
2018-12-12 19:03:32 +0000

    Added check if len of list is zero

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,10 +3,10 @@
 
 def delete_at(my_list=[], idx=0):
 
-    if not my_list:
+    if my_list is None or len(my_list) == 0:
         return None
 
-    if idx <= 0 or idx > len(my_list) - 1:
+    if idx < 0 or idx > len(my_list) - 1:
         return my_list
 
     del my_list[idx]
