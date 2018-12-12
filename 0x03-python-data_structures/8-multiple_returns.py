e738f82da222479ef4566d27022dcf61e5a9b230
ConnorBrereton <breretonconnor@gmail.com>
2018-12-12 05:20:14 +0000

    Returns the size of a sentence and it's first character

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def multiple_returns(sentence):
+
+    if sentence is None:
+        char = None
+
+    else:
+        char = sentence[0]
+
+    return (len(sentence), char) 
