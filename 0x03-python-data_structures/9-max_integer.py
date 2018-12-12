64155a898f41fae9416c47edae17e83820cb8671
ConnorBrereton <breretonconnor@gmail.com>
2018-12-12 05:50:35 +0000

    Finds the max value in a list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def max_integer(my_list=[]):
+
+    if my_list is None:
+        return None
+
+    base = my_list[0]
+
+    for i in my_list:
+        if i > base:
+            base = i
+
+    return (base)
