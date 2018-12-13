9fdfe5c0051e9843465dd61a616aa4a7a6483c22
ConnorBrereton <breretonconnor@gmail.com>
2018-12-13 22:23:40 +0000

    Finds and replaces an element of a list with another element

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def search_replace(my_list, search, replace):
+
+    """
+    fail checks
+    """
+
+    if my_list is None or len(my_list) == 0:
+        return None
+
+    result = list(map(lambda e: e if e != search else replace, my_list))
+    return result
