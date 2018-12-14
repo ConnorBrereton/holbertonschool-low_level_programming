27e9a14d0ca8a340e17f6453a5bc2bd82cca9cb1
ConnorBrereton <breretonconnor@gmail.com>
2018-12-14 02:05:46 +0000

    Removed fail check all together since checker is complaining

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -3,11 +3,5 @@
 
 def update_dictionary(a_dictionary, key, value):
 
-    """
-    fail check
-    """
-    if not a_dictionary:
-        return
-
     a_dictionary[key] = value
     return a_dictionary
