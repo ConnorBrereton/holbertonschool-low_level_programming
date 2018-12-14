65e6ae8cb664f8761ffd61fcdf3e3c1428047389
ConnorBrereton <breretonconnor@gmail.com>
2018-12-14 00:35:05 +0000

    Fixed fail check issue of empty set

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -6,7 +6,7 @@ def only_diff_elements(set_1, set_2):
     """
     fail checks
     """
-    if not set_1 or not set_2:
-        return {}
+    if set_1 is None or set_2 is None:
+        return None
 
     return set_1 ^ set_2
