e7383f8f0210188aca6f21f2534a5e1d31efa5b0
ConnorBrereton <breretonconnor@gmail.com>
2018-12-14 02:23:38 +0000

    Removes a key from a dictionary

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def simple_delete(a_dictionary, key=""):
+
+    """
+    fail check
+    """
+    if not a_dictionary:
+        return
+
+    if key in a_dictionary:
+        del a_dictionary[key]
+
+    return a_dictionary
