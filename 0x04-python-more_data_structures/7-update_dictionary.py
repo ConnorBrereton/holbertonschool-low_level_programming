839497c71c971d08eb881f00cbfef9a71e741c6b
ConnorBrereton <breretonconnor@gmail.com>
2018-12-14 02:02:33 +0000

    Updates a keys value in a dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def update_dictionary(a_dictionary, key, value):
+
+    """
+    fail check
+    """
+    if not a_dictionary:
+        return {}
+
+    a_dictionary[key] = value
+    return a_dictionary
