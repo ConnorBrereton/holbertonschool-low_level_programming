4a12e86128ac2ba7006246d9af0e1d2281616fac
ConnorBrereton <breretonconnor@gmail.com>
2019-01-04 05:39:45 +0000

    Accesses and updates private attributes

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Module that contains the Square class"""
+
+class Square:
+    """Initialize the Square instance
+
+    Args:
+        size: The size of the Square instance
+    """
+
+    def __init__(self, size=0):
+        self.size = size
+
+
+    """Getter method for size attribute
+
+    Returns:
+        Private attribute `size`
+    """
+    @property
+    def size(self):
+
+        return self.__size
+
+
+    """Setter method for size attribute
+
+    Returns:
+        Private attribute `size`
+    """
+    @size.setter
+    def size(self, value):
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+
+    """Handles computation
+
+    Returns:
+        Computed area attribute `size`
+    """
+    def area(self):
+
+        return self.__size ** 2
