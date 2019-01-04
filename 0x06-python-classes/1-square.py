38dcbf9f6d55a4eae6475e81b21f10be2f70d5a5
ConnorBrereton <breretonconnor@gmail.com>
2019-01-04 03:34:56 +0000

    Initializes the size field to be a private instance field

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines the square"""
+
+
+class Square:
+    """Initialize the Square instance
+
+    Args:
+        size: The size of the Square instance
+    """
+
+    def __init__(self, size):
+        self.__size = size
