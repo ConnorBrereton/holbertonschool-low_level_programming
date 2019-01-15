8bc7c713507b6443b0ad7b47b317d0d000835d70
ConnorBrereton <breretonconnor@gmail.com>
2019-01-15 04:41:51 +0000

    Iteratively prints all key:value pairs

---
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints all key:value pairs
+ *
+ * @ht: Pointer to hashtable
+ *
+ * Return: n/a
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0;
+
+	int flag = 1;
+
+	hash_node_t *cur;
+
+	if (!ht || !(ht->array))
+		printf("{}");
+
+	printf("{");
+
+	while (i < ht->size) /* loop through array */
+	{
+		cur = ht->array[i]; /* loop through each array linked list */
+
+		while (cur)
+		{
+			if (flag == 0)
+				printf(", ");
+
+			printf("\'%s\': \'%s\'", cur->key, cur->value);
+
+			cur = cur->next;
+			flag = 0; /* reset each iteration to ensure comma-separated K:V pairs */
+		}
+		i++;
+	}
+
+	printf("}\n");
+}
