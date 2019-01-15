584a401ef4b024025bbaa1703e169d42fe8ef5ba
ConnorBrereton <breretonconnor@gmail.com>
2019-01-15 02:53:06 +0000

    Implementation of DJB2 algorithm

---
+++ 0x19-hash_tables/1-djb2.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - DJB2 hash function
+ *
+ * @str: String to be hashed
+ *
+ * Return: Hashed representation of string
+ */
+
+unsigned long hash_djb2(const unsigned char *str)
+{
+    unsigned long int hash;
+    int c;
+
+    hash = 5381;
+    while ((c = *str++))
+    {
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+    }
+    return (hash);
+}
