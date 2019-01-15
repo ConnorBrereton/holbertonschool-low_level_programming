f195c40544533679664a18827308924dce4e7654
ConnorBrereton <breretonconnor@gmail.com>
2019-01-15 21:37:17 +0000

    Changed deletion case to free the entire table

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -9,7 +9,7 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned int i;
+	unsigned long int i;
 	hash_table_t *new_table;
 
 	if (size == 0)
@@ -26,7 +26,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (!(new_table->array))
 	{
-		free(new_table->array);
+		free(new_table);
 		return (NULL);
 	}
 
