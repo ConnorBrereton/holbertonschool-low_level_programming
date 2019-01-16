8bcc7453149c8a39cadfb3003d643400a01f66ed
ConnorBrereton <breretonconnor@gmail.com>
2019-01-16 00:39:15 +0000

    Cleanup hashtable function

---
+++ 0x19-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+
+	hash_node_t *next_index;
+
+	if (!ht || !(ht->array) || ht->size == 0)
+		return;
+
+	for (i = 0; i < ht->size; i++) /* go through array */
+	{
+		while (ht->array[i])
+		{
+			next_index = ht->array[i]->next; /* sets pointer to next index */
+			free(ht->array[i]->key); /* break link from array index to nodes key */
+			free(ht->array[i]->value); /* break link from array index nodes value */
+			free(ht->array[i]); /* free array index */
+			ht->array[i] = next_index; /* resets for next index */
+		}
+	}
+	free(ht->array); /* free the ht-pointer to the array */
+	free(ht); /* free the link to the table */
+}
